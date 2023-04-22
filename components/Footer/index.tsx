d76a38db656a9638c446d12272f24dc3916064a3
smoothpie <kupriyanovichk72@gmail.com>
2023-04-23 01:22:46 +0200

    Fixed linkedin link

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
         <a href="https://www.chillsubs.com">Chill Subs</a>
         <a href="https://teal.tools">Hire me</a>
         {/* <a href="https://teal.tools">Relocate me</a> */}
-        <a href="https://linkedin.com/karinakupp">LinkedIn</a>
+        <a href="https://linkedin.com/karina-kupp">LinkedIn</a>
         <a href="https://instagram.com/karinakupp">Instagram</a>
         {/* <a href="https://instagram.com/karinakupp">Let's be friends please</a> */}
       </div>
